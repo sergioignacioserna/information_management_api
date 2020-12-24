9f4f089c6a0a6187b0bad26e6c3af7be3baf3131
David Gomez <dg.dave@gmail.com>
2020-12-24 15:26:14 -0500

    Base de datos Postgres en Heroku.

--- db/ConectionDB.py
+++ db/ConectionDB.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Creando Motor y Conexion con la Base de Datos
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/G3M3E10"
-#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@ec2-3-231-48-230.compute-1.amazonaws.com:5432/G3M3E10"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/G3M3E10"
+SQLALCHEMY_DATABASE_URL = "postgres://lixrdwmnbqhpup:05942cce3e25d8464645552e5cc042dafdbeb810c0422ca60d34603c412170a4@ec2-3-211-48-92.compute-1.amazonaws.com:5432/d4jc6kqk0i4onu"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # Creacion de la Sesion
